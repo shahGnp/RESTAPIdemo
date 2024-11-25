a849805465902aff1ee8dd6dd3842cb6af7a7ee9
shahGnp <gauravshahfromnepal@gmail.com>
2024-11-25 22:12:19 +0545

    server mod

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+def create_item(item: Item):
+    return item
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
